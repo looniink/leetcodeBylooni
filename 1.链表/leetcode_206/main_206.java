49f510094c4e2b61a0d07eff39319fcd98dd7313
looniink <jianbin.lu@foxmail.com>
2020-07-23 14:14:48 +0800

    :tada:每天进步一点点噢

--- 1.链表/leetcode_206/main_206.java
+++ 1.链表/leetcode_206/main_206.java
@@ -16,11 +16,11 @@ public class main_206 {
 		ListNode pre = null;
 		ListNode temp = null;
 		ListNode cur = head;
-		while (cur != null) {
-			temp = cur.next;
+		while (cur != null) {     //当cur到达尾部时退出循环，链表反转完成
+			temp = cur.next;      //预存cur.next节点
 			cur.next = pre;       //改变指针的位置达到反转链表的目的
-			pre = cur;
-			cur = temp;
+			pre = cur;            //反转指针后将pre指向下一个cur节点
+			cur = temp;           //cur指向下一个节点
 		}
 		return pre;
 	}
