0e1d247a6c79764016a6d6318266a50540ca5883
looniink <jianbin.lu@foxmail.com>
2020-08-20 13:55:11 +0800

    :triangular_flag_on_post:树的前序遍历

--- 4.树/leetcode_589/main_589.java
+++ 4.树/leetcode_589/main_589.java
@@ -1,17 +1,14 @@
 package leetcode_589;
 
-import kotlin.reflect.jvm.internal.impl.descriptors.deserialization.PlatformDependentDeclarationFilter;
-import org.w3c.dom.Node;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 
 /**
  * @author :lujianbin
  * @create :2020-08-19 12:20:00
- * @description :树的前序遍历
+ * @description :树的前序遍历，先遍历根节点再遍历左右的孩子节点
  */
 @SuppressWarnings("all")
 public class main_589 {
@@ -43,7 +40,7 @@ public class main_589 {
 	private void dfs(Node root, List<Integer> res) {
 		if (root == null)
 			return;
-		res.add(root.val);
+		res.add(root.val);      //先遍历根节点
 		for (var child : root.children) {       //此处应为Node child,java 10过后可以用var变量来动态表示任意类型
 			//var类型会根据所赋的值自动判断
 			dfs(child, res);
