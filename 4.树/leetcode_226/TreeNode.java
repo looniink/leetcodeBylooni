bc81e27c4bafbd451527d02162390b29deae7f9e
looniink <jianbin.lu@foxmail.com>
2020-08-26 19:38:51 +0800

    :triangular_flag_on_post:二叉树的翻转

--- 4.树/leetcode_226/TreeNode.java
+++ 4.树/leetcode_226/TreeNode.java
@@ -3,12 +3,12 @@ package leetcode_226;
 /**
  * @author :lujianbin
  * @create :2020-08-26 19:04:00
- * @description :
+ * @description :二叉树结构
  */
 public class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+	int val;            //中间的值
+	TreeNode left;      //左孩子
+	TreeNode right;     //右孩子
 
 	TreeNode(int x) {
 		val = x;
