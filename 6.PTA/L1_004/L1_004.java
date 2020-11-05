0410081bba7d9cbc5666c79aef4893582b0c28db
looniink <jianbin.lu@foxmail.com>
2020-11-05 21:39:11 +0800

    :triangular_flag_on_post:L1_004(计算华氏温度)

---
+++ 6.PTA/L1_004/L1_004.java
@@ -0,0 +1,17 @@
+package L1_004;
+
+import java.util.Scanner;
+
+/**
+ * @author :lujianbin
+ * @create :2020-11-05 21:11:00
+ * @description :¼ÆËã»ªÊÏÎÂ¶È
+ * @solution :
+ */
+public class L1_004 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		System.out.println("Celsius = " + 5 * (i - 32) / 9);
+	}
+}
