f14504c3cea6c2e72d5e2facef3670860e422627
looniink <jianbin.lu@foxmail.com>
2020-11-13 12:06:13 +0800

    :triangular_flag_on_post:L1_012(计算指数)

---
+++ 6.PTA/L1_012/Main.java
@@ -0,0 +1,17 @@
+package L1_012;
+
+import java.util.Scanner;
+
+/**
+ * @author :lujianbin
+ * @create :2020-11-13 11:52:00
+ * @description :¼ÆËãÖ¸Êý
+ * @solution :
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int nextInt = scanner.nextInt();
+		System.out.println("2" + "^" + nextInt + " = " + (int) Math.pow(2, nextInt));
+	}
+}
