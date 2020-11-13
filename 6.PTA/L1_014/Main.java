e28c274b5ae739b507746586f1b5012a792383f0
looniink <jianbin.lu@foxmail.com>
2020-11-13 18:05:29 +0800

    :triangular_flag_on_post:L1_013(Simple Problem)

---
+++ 6.PTA/L1_014/Main.java
@@ -0,0 +1,13 @@
+package L1_014;
+
+/**
+ * @author :lujianbin
+ * @create :2020-11-13 18:03:00
+ * @description :
+ * @solution :
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This is a simple problem.");
+	}
+}
