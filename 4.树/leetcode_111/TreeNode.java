32696b1a09c6d5652e028faf2d07eef787968264
looniink <jianbin.lu@foxmail.com>
2021-01-22 17:59:30 +0800

    🚩二叉树的最小深度

---
+++ 4.树/leetcode_111/TreeNode.java
@@ -0,0 +1,18 @@
+package leetcode_111;
+
+/**
+ * @author :lujianbin
+ * @create :2020-10-27 17:00:00
+ * @description :
+ */
+public class TreeNode {
+	//节点值，左孩子，右孩子
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	//构造方法
+	TreeNode(int x) {
+		val = x;
+	}
+}
--- 4.树/leetcode_111/leetcode_111.java
+++ 4.树/leetcode_111/leetcode_111.java
@@ -4,10 +4,30 @@ package leetcode_111;
  * @author :lujianbin
  * @create :2021-01-22 12:24:00
  * @description :二叉树的最小深度
- * @solution :
+ * @solution :采用dfs递归的方法
  */
 public class leetcode_111 {
-	public static void main(String[] args) {
 
+	public int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		//递归的结束条件
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+
+		//左右子树递归
+		int left = minDepth(root.left);
+		int right = minDepth(root.right);
+
+		//左子树为空或者右子树为空时
+		if (root.left == null || root.right == null) {
+			return left + right + 1;
+		}
+
+		//当左右子树都不为空的时候
+		return Math.min(left, right) + 1;
 	}
 }
